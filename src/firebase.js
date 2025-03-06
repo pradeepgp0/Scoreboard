3d3007c532dd51c2550c77cd6f8451ffcca7c096
Pradeep GP <pradeepgp8@gmail.com>
2025-03-06 11:24:51 +0530

    bg change

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth"; 
 import { getFirestore, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,5 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 const db = getFirestore(app);
 export { db, doc, setDoc, getDoc, updateDoc };
+
+
